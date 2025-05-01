893820ba738571ddedfc28a9e6780a93f6796ca1
Gregor Niehl <gregorniehl@web.de>
2025-05-01 19:53:21 +0200

    python: Rename motors_dev to motors

--- crawler/setup.py
+++ crawler/setup.py
@@ -22,7 +22,7 @@ setup(
         "console_scripts": [
             "crawler_web_api = crawler.web_api:main",
             "crawler_blinker = crawler.blinker:main",
-            "crawler_motors = crawler.motors_dev:main",
+            "crawler_motors = crawler.motors:main",
             "crawler_encoders = crawler.encoders:main",
         ],
     },
